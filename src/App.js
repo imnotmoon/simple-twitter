66d4fcf8c05008bd415efd6571c397aff037e3fd
imnotmoon <imnotmoon@gmail.com>
2021-03-13 16:31:48 +0900

    useTabs : hook 객체를 리턴

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 
+/// useInput
 const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
 
@@ -30,6 +31,44 @@ export default App = () => {
       <h2>Start editing to see some magic happen!</h2>
       <button onClick={incrementItem}>Increment</button>
       <button onClick={decrementItem}>Decrement</button>
+    </div>
+  );
+};
+
+
+/// useTabs
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+};
+
+export default App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <br />
+      <div>{currentItem.content}</div>
     </div>
   );
 };
