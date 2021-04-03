daaca13626a8adaa14a4bc46cbdbd0b1c9638fc9
imnotmoon <imnotmoon@gmail.com>
2021-04-03 17:42:25 +0900

    firestore : create, read

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,60 @@
-import React from 'react'
-import { authService } from '../fbInstance'
+import React, { useState, useEffect } from 'react'
+import { authService, dbService } from '../fbInstance'
 
 function Home() {
 
+    const [tweet, setTweet] = useState("")
+    const [tweets, setTweets] = useState([])
+
+    const getTweets = async () => {
+        const rows = await dbService.collection("Tweets").get()
+        rows.forEach(document => {
+            const tweetObject = {
+                ...document.data(),
+                id: document.id,
+            }
+            setTweets((prev) => [tweetObject, ...prev])
+        })
+        console.log(tweets)
+    }
+
+    useEffect(() => {
+        getTweets()
+    }, [])
+
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        await dbService.collection("Tweets").add({
+            tweet,
+            createdAt: Date.now(),
+        })
+        setTweet("")
+    }
+
+    const onChange = (event) => {
+        const { target: { value } } = event
+        setTweet(value)
+    }
+
     return (
         <div>
-            Home
-            <br />
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder="What's on your mind?"
+                    max={120}
+                    value={tweet}
+                    onChange={onChange}
+                />
+                <input type="submit" name="" value="Tweet" />
+            </form>
+            <div>
+                {tweets.map(item => (
+                    <div key={item.id}>
+                        <h4>{item.tweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
